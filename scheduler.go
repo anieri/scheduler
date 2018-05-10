7adc42bc6aec70740d5bb5650e85d752c4c4feac
anieri <andre@nieri.com.br>
2018-05-09 21:14:31 -0300

    Running scheduled jobs

--- scheduler.go
+++ scheduler.go
@@ -1,6 +1,10 @@
 package scheduler
 
-import "time"
+import (
+	"time"
+
+	"go.uber.org/ratelimit"
+)
 
 type ScheduleJob interface {
 	Run(time.Time) error
@@ -14,7 +18,7 @@ type schedule struct {
 }
 
 type Scheduler struct {
-	jobs []schedule
+	jobs []*schedule
 }
 
 func New() *Scheduler {
@@ -22,10 +26,24 @@ func New() *Scheduler {
 }
 
 func (sched *Scheduler) Add(interval time.Duration, job ScheduleJob) {
-	(*sched).jobs = append(sched.jobs, schedule{job, interval, time.Now()})
+	(*sched).jobs = append(sched.jobs, &schedule{job, interval, time.Now()})
 }
 
 func (sched *Scheduler) Run() {
+	for _, job := range sched.jobs {
+		(*job).last = time.Now()
+	}
+	rl := ratelimit.New(1)
+	for {
+		now := rl.Take()
+
+		for _, job := range sched.jobs {
+			if now.Before(job.last.Add(job.interval)) {
+				continue
+			}
+			go job.job.Run(now)
+		}
+	}
 }
 
 func (sched *Scheduler) Stop() {
